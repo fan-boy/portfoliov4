1fd8c8ff44e4b548e64549b959f9fe77e2f91dca
Aaditya Shete <aadityashete@Aadityas-MacBook-Pro.local>
2025-06-21 20:39:07 -0400

    metadata

--- Frontend/src/app/layout.tsx
+++ Frontend/src/app/layout.tsx
@@ -6,6 +6,16 @@ import CustomCursor from './components/customcursor';
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
+
+
+export const metadata = {
+  title: "Aaditya Shete",
+  description: "Product Designer | Developer",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
